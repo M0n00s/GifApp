4116e4cb7091c3b5a70fc1f44bad358042c6ce15
M0n00s <programacion.buffalo@gmail.com>
2023-02-26 15:46:54 -0400

    ADD / Boostrap and styles

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,8 +1,12 @@
 export const GifItem = ({ url, title }) => {
   return (
-    <div className="card">
-      <p>{title}</p>
-      <img src={url} alt="img" />
-    </div>
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={url} className="card-img-top" alt={title} />
+        <div className="card-body ">
+          <p className="card-text">{title}</p>
+        </div>
+      </div>
+    </>
   );
 };
